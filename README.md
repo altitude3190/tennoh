406248e7735b568c505ad881bc98e6baf17c7b63
Hodaka Suzuki <altitude3190@gmail.com>
2019-06-05 13:46:12 +0900

    pdf unlocker script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# tennoh
+# pdf_password_unlocker
+
+```
+bash pdf_unlocker.sh [dir_name]
+
+# dir_name: directory name which places pdf files
+            if it is undefined, the target directory becomes '.'
+```
+
+For all target pdf files:
+
+1. add prefix "encrypted_" to the pdf file name
+2. output decrypted one as original pdf file name
---
+++ pdf_unlocker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash -eu
+
+if ! type qpdf > /dev/null 2>&1; then
+    echo "please install qpdf with `brew install qpdf`"
+    exit 1
+fi
+
+read -sp "PDF Password: " pass
+tty -s && echo
+
+# thank you for teaching me such a nice syntax
+# https://qiita.com/m-yamashita/items/889c116b92dc0bf4ea7d
+# if $1 is defined, then dir=$1, else dir='.'
+dir=${1:-'.'}
+cd $dir
+
+for file in `ls | grep '.pdf'`; do
+    mv "$file" "encrypted_$file"
+    qpdf --password=$pass --decrypt "encrypted_$file" $file
+done
